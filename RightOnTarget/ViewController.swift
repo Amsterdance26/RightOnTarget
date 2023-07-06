efbbb2e53bcfc97213ee2aa56eb9f0de039c7d21
Amsterdance26 <amsterdance26@gmail.com>
2023-07-06 21:12:12 +0700

    Initial Commit

---
+++ RightOnTarget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RightOnTarget
+//
+//  Created by Василий Васильевич on 06.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
